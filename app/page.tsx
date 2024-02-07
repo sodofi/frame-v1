0b6e58c63254126fc24291b47cebddb112d9691d
sophiahdew <sophiahdew@gmail.com>
2024-02-07 16:56:39 -0600

    endpoint api update

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ const frameMetadata = getFrameMetadata({
       label: "Begin"
     }
   ],
-  image: `${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/Qme4FXhoxHHfyzTfRxSpASbMF8kajLEPkRQWhwWu9pkUjm/0.png`,
+  image: `${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmaGGzBaZZ2ZiGEuKsgGXWRxUfdGLnnG5WDNs7XZ8A6nGS/0.png`,
   post_url: `${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=1`,
 });
 
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: 'Cosmic Cowboys',
     description: 'A frame telling the story of Cosmic Cowboys',
-    images: [`${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/Qme4FXhoxHHfyzTfRxSpASbMF8kajLEPkRQWhwWu9pkUjm/0.png`],
+    images: [`${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmaGGzBaZZ2ZiGEuKsgGXWRxUfdGLnnG5WDNs7XZ8A6nGS/0.png`],
   },
   other: {
     ...frameMetadata,
